b35e7d293fad34881e49d2f2b78b359d3975b8b2
BarsonyBotond <barsony99.botond@gmail.com>
2023-01-14 17:03:30 +0100

    First Lecture

---
+++ Alapok/Alapok/Controll_structions.cs
@@ -0,0 +1,132 @@
+﻿/*Console.WriteLine("Hello World"); //itt nem lehet csak egyben lehet lebegő utasítás
+//elágazás
+int ammo = 123;
+bool IsInAir = false;
+
+if(ammo>0 && !IsInAir=false)
+        {
+    Console.WriteLine("BOOM");
+};
+bool canshoot = if(ammo>0 && !IsInAir=false);
+{
+    Console.WriteLine("BOOM");
+};
+else
+{
+    Console.WriteLine("click");
+}
+//switch
+    //___________________________________
+Console.WriteLine("írj be egy egész számot!");
+string line = Console.ReadLine();
+int number = int.Parse(line);
+Console.WriteLine("A szám duplája");
+Console.WriteLine(number * 2);*/
+//___________________________
+//páros vagy páratlan?
+/*Console.WriteLine("páros vagy páratlan?")
+    string line = Console.ReadLine();
+    int number = int.Parse(line);
+Console.WriteLine(int number  )*/
+//jó megoldás:
+/*bool isEveneornot = ???;
+if(isEveneornot)
+{
+    Console.WriteLine("páros"); 
+        }
+bool is Even number % 2
+string line= Console.ReadLine();
+int number = int.Parse(line);
+else
+{
+    Console.WriteLine("páratlan");*/
+
+//pozítív negatív
+/*string line = Console.ReadLine();
+int number int.Parse();
+
+if (number > 0)
+
+{
+    Console.WriteLine("pozítív");
+}
+else if (number > 0)
+{
+    Console.WriteLine("negatív");
+}
+else
+{
+    Console.WriteLine("zéró");
+}
+*/
+//kerek nemkerek
+/*using System.Globalization;
+
+Console.WriteLine("írj be egy számot");
+string line = Console.ReadLine();
+int number = int.Parse(line);
+string text = number % 10 == 0 ? "KEREK" : "Nem Kerek";
+Console.WriteLine(text);
+//___________________________________
+//ciklusok
+int i = 1;
+while (i <= 100) 
+{
+    Console.WriteLine(i);
+    i++;
+        }
+//ugyanez csökenő?
+for int i=1 (int j; = 1 j <= 100; j++);
+{
+    Console.WriteLine(j);
+    j++;
+}*/
+/*int k = 3;
+while (k <= 9)
+{ Console.WriteLine(k); k = k + 3; }k
+{ Console.WriteLine(k); k = k + 3; }k
+int l = 3;
+while (l <= 10) ;
+modulo = l % 3 == 0;*/
+//___________________
+/*Console.WriteLine("írj be egy számot!");
+
+
+string line = Console.ReadLine();
+int n = int.Parse(line);
+int modulo=n % 5;
+if (n <= 0);
+else
+{
+    int modulo = n % 7; 
+        int( n !> 0);
+    if (n > 0);
+}
+{
+    Console.WriteLine("n");
+}
+*/
+//_______________________________
+//szorzótábla
+/*int p;
+int o;
+int p = 1;
+int o = 1;
+    {
+    Console.WriteLine("p");
+    Console.WriteLine("o");
+}
+int e = 1;
+{ for (int w = 0; w < 10; w++);
+
+{for (int w = 1; w =< 10; w++);   
+    
+}
+} */
+//metódusok 
+float f2 = 12.5f;
+float f3 = 3f;
+double power = Math.Pow(f2, f3);
+{
+    Console.WriteLine("power");
+}
---
+++ Alapok/Alapok/Program.cs
@@ -0,0 +1,68 @@
+﻿//Console.WriteLine("Hello World!");
+//Kiiratas
+/*igy is lehet akar tobb sor is kommentelni */
+/*int Age = 16;
+Console.WriteLine(Age);
+Age = 17;
+    Console.WriteLine(Age);
+int height; //deklaráció
+height = 191; //defíníció
+Console.WriteLine(height);
+int a = Age + height-5;
+Console.WriteLine(a);
+int x = 345, y = -590, z = 7;
+x = y - z;
+Console.WriteLine(x);
+// műveletek
+int sum = z + y;
+int dif = x - z;
+int product = y * 2 * z;
+int rate = x + y;
+int modulo = 10 % 3; //1
+int modulo1 = 12 % 4; //0
+int modulo2 = 6 % 5; //1
+int modulo3 = 12 % 16; //12
+int num1 = 3+2*4; //van műv. sorrend, lehet zárójelezni is
+int number = 0;
+ number = number + 1; //1
+ number += 1; // ugyanaz mint előző
+ number -= 2;
+ number *= 6;
+ number /= 3;
+ number %= 25; //olyan mint modulo
+number = ++ // ugyanaz mint +=1
+//______________________________
+int = f1 = 5;
+float f1 = 12.5f; //tud törtet is (32 biten)
+double d1 = 12.77; // itt nem kell f (64 biten) az int pedig szintén 32, van 64 bit long
+float f2 = 12.45f;
+int d2 = d1+f2; //osztásnál nincs lecsapás
+//casting (kasztolás) egyik tipús másik típús átalakít
+float f4 = 33.678921f;
+int i2 = (int)f4;//explicit kasztolás
+float f5 = i2 ; //implicit
+string Wondrus_String = "vastaghas";
+string s1 = "aaaa" + Wondrus_String + "" + " " + i2.ToString();
+string s2 = "8" + "4" + "aaaa";
+
+int xxx = int.Parse("10");
+xxx += 5;
+int hex = 0x1A4B;
+    int bin = 0b10100001110;
+//_______________________________________
+bool b1 = false;
+bool b2 = true;
+bool b3 = i2 > f1; //igaz vagy hamis
+bool b4 = i2 <= f4; //kissebb_nagyobb_egyenlő
+bool b9 = b2 == b3; //egyenlőségvizsgálat
+bool b10 = b3 != b4; // lehet stringeken is csinálni != és ==
+bool b13 = "aaagddb" == "bshfghgqe"; //false 
+Console.WriteLine(s2+ s1);
+//_______________________________________________
+bool b14 = b1 == b2; //== és != működik boolon is
+//2 boolból kimenet is bool és és a vagy
+bool b15 = b1 && b2; //és művelet &&fontos & mást jelent!
+bool b16 = b1 || b2;// vagy művelet
+bool b17 = !b1; //negálás 
+bool b18 = b1 ^ b2; //XOR:kízáró vagy */
+
